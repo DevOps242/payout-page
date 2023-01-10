3f4f2c910e0c371802276c6d9bcb85e177475ce2
Akeem Palmer <akeemk.palmer@gmail.com>
2023-01-10 16:08:56 -0500

    Update: UI

--- src/components/ui/Button/Dropdown/DropdownCard.js
+++ src/components/ui/Button/Dropdown/DropdownCard.js
@@ -3,25 +3,24 @@ import Button from "../Button";
 
 const DropdownCard = ({ options, action, tabs }) => {
   return (
-    <>
-      <div class="container flex justify-center">
-        <div class="dropdown-container block rounded-lg shadow-lg bg-white max-w-sm">
-          {tabs.map((item) => {
-            if (action) {
-              return (
-                <>
-                  <div className="dropdown-menu-container container mb-3">
-                    <h4 className="dropdown-menu" onClick={item.onPressAction}>
-                      {item.title}
-                    </h4>
-                  </div>
-                </>
-              );
-            }
-          })}
-        </div>
+    <div className="container flex justify-center">
+      <div className="dropdown-container block rounded-lg shadow-lg bg-white max-w-sm">
+        {tabs.map((item) => {
+          if (action) {
+            return (
+              <div
+                key={Math.random() * (100000 - 1) + 1}
+                className="dropdown-menu-container container mb-3"
+              >
+                <h4 className="dropdown-menu" onClick={item.onPressAction}>
+                  {item.title}
+                </h4>
+              </div>
+            );
+          }
+        })}
       </div>
-    </>
+    </div>
   );
 };
 
